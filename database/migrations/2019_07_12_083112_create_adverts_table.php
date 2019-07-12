a2bd5b20cd7d5f45eacfe8c246db03e2b6da78dc
huzairuje <metalh8@gmail.com>
2019-07-12 15:37:39 +0700

    add migration on feature

---
+++ database/migrations/2019_07_12_083112_create_adverts_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdvertsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('adverts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 191);
+            $table->dateTime('publish_datetime');
+            $table->string('featured_image', 191);
+            $table->text('content');
+            $table->string('meta_title', 191)->nullable();
+            $table->string('cannonical_link', 191)->nullable();
+            $table->string('slug', 191)->nullable();
+            $table->text('meta_description', 65535)->nullable();
+            $table->text('meta_keywords', 65535)->nullable();
+            $table->enum('status', ['Published', 'Draft', 'InActive', 'Scheduled']);
+            $table->bigInteger('advert_category_id')->unsigned();
+            $table->bigInteger('created_by')->unsigned();
+            $table->bigInteger('updated_by')->unsigned()->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('adverts');
+    }
+}
