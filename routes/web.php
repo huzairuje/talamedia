15aaef00f68f157a84796ffda028911e8d4a9ff3
ujehuzair <metalh8@gmail.com>
2019-08-20 01:59:04 +0700

    disable register, reset, and verify Auth Route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@ use Illuminate\Support\Facades\Auth;
 |
 */
 
-Auth::routes();
+Auth::routes([
+    'register' => false, // Registration Routes...
+    'reset' => false, // Password Reset Routes...
+    'verify' => false, // Email Verification Routes...
+]);
 
 Route::get('/', function () {
    return redirect()->route('login');
